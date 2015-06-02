27e32bd987098907d70936ceb62bd4bceee278ef
oracle <oracle@atanasoft17.danielcastelao.org>
2015-06-02 12:01:35 +0200

    Commit final

--- test/comprobacionErros/CalculoTest.java
+++ test/comprobacionErros/CalculoTest.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-package comprobacion;
+package comprobacionErros;
 
 import comprobacionErros.Calculo;
 import org.junit.After;
--- test/mcdbateriaParam/CalculoTest.java
+++ test/mcdbateriaParam/CalculoTest.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-package mcdbateria;
+package mcdbateriaParam;
 
 import mcdbateriaParam.Calculo;
 import java.util.Arrays;
