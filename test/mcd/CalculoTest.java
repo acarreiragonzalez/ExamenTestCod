4745116a6cd5692e65668842d57cfa35e4af27e9
oracle <oracle@atanasoft17.danielcastelao.org>
2015-06-02 10:32:23 +0200

    Realizado primeiro test,sólo do metodo  obter MCD

---
+++ test/mcd/CalculoTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mcd;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author oracle
+ */
+public class CalculoTest {
+    
+    public CalculoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+   
+
+    /**
+     * Test of obtenerMCD method, of class Calculo.
+     */
+    @Test
+    public void testObtenerMCD() {
+        System.out.println("obtenerMCD");
+        int num1 = 0;
+        int num2 = 0;
+        Calculo instance = new Calculo();
+        int expResult = 0;
+        int result = instance.obtenerMCD(num1, num2);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
